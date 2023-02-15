5e9255da74fdc6aa02e1582cf83c5f24797baf36
Odilbek <odilbekjuraev07@gmail.com>
2023-02-15 14:36:56 +0500

    some changes done

---
+++ lib/data/models/file_model/file_model.dart
@@ -0,0 +1,29 @@
+class FileModel {
+  String fileName;
+  String fileUrl;
+
+  FileModel({required this.fileName, required this.fileUrl});
+}
+
+List files = [
+  FileModel(
+    fileName: "Ronaldo.jpg",
+    fileUrl:
+    "https://images.beinsports.com/ALZ9-lFz8NdnCwI0eXadFKaFcUs=/full-fit-in/1000x0/4574680-2023-01-03T174530Z_1779681513_UP1EJ131DBSBQ_RTRMADP_3_SOCCER-SAUDI-RONALDO.JPG",
+  ),
+  FileModel(
+    fileName: "Kiber asoslar.pdf",
+    fileUrl:
+    "https://dl.tuit.uz/subject-resources/73105/UPCxnK3sjeGBjXMQuwJGCb38nQqwFS3Nx87fvkvI.pdf",
+  ),
+  FileModel(
+    fileName: "Amaliy ish.pdf",
+    fileUrl:
+    "https://dl.tuit.uz/subject-resources/70762/ZJlQJWZXGyCx5bXLhgltT6DroGT6XugruK6ZRNel.docx",
+  ),
+  FileModel(
+    fileName: "image.jpg",
+    fileUrl:
+    "https://media.istockphoto.com/id/1189860501/photo/traveler-with-a-backpack-standing-on-a-mountain-peak-above-clouds-3d-render-illustration.jpg?",
+  ),
+];
